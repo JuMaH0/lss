1db2fdccb1038f851358af07c0b1e065f8f46278
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2018-04-28 22:43:53 +0200

    update

--- MissionFilter.user.js
+++ MissionFilter.user.js
@@ -15,38 +15,37 @@
     $(".navbar-right").append('<li><a id="black"><div id="black_circle" style="background-color: black;' + circle + '"></div></a></li>');
 
     $("#black").click(function() {
-
         if($('#black_circle').css('background-color') == 'rgb(0, 0, 0)'){
             $("#black_circle").css({ 'background-color':'grey'});
+        }
+
+        var mission = ["Flächenbrand","Taschendiebstahl","Brennender Bus","Person im Aufzug","Brand in Baumarkt"];
+        var mission_count = mission.length;
+        var count_missions = $(".missionSideBarEntry").length;
+        var strainer = $('[id^=mission_caption_]');
+        var mission_id;
+        var x;
+        var y;
 
 
-            var mission = ["Flächenbrand","Taschendiebstahl","Brennender Bus","Person im Aufzug"];
-            var mission_count = mission.length;
-            var count_missions = $(".missionSideBarEntry").length;
-            var strainer = $('[id^=mission_caption_]');
-            var mission_id;
-            var x;
-            var y;
+        for (x = 0; x < count_missions; x++) {
 
+            mission_id = $(strainer[x]).attr('id').match(/[0-9]+/);
+            var cull = document.getElementById('mission_caption_'+mission_id+'').innerHTML;
+            var present = cull.indexOf(mission[0]);
 
-            for (x = 0; x < count_missions; x++) {
-
-                mission_id = $(strainer[x]).attr('id').match(/[0-9]+/);
-                var cull = document.getElementById('mission_caption_'+mission_id+'').innerHTML;
-                var present = cull.indexOf(mission[0]);
-
-                for (y = 0; y < count_missions; y++) {
-                    if(present > -1){
-                        document.getElementById('mission_'+mission_id+'').style.display = "block";
-                    } else {
-                        present = cull.indexOf(mission[y]);
-                        document.getElementById('mission_'+mission_id+'').style.display = "none";
-                    }
-
+            for (y = 0; y < count_missions; y++) {
+                if(present > -1){
+                    document.getElementById('mission_'+mission_id+'').style.display = "block";
+                } else {
+                    present = cull.indexOf(mission[y]);
+                    document.getElementById('mission_'+mission_id+'').style.display = "none";
                 }
 
             }
+
         }
+
     });
 
 })
