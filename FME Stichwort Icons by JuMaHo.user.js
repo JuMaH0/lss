180dcb81dc673c36b3ac94ce8e8c62c1287da07e
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2018-09-30 16:53:19 +0200

    FME Stichwort Icons by JuMaHo

---
+++ FME Stichwort Icons by JuMaHo.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         FME Stichwort Icons by JuMaHo
+// @namespace    Leitstellenspiel
+// @version      0.5
+// @description  Icon resize
+// @author       JuMaHo
+// @match        https://www.leitstellenspiel.de/
+// ==/UserScript==
+
+(function() {
+    'use strict';
+///Größe der Einsatzgrafiken
+    $(".mission_vehicle_state").css({ 'min-width': '30px', 'min-height': '42px' });
+    var tid = setInterval(buttonsize, 5000);
+    function buttonsize() {
+        $(".mission_vehicle_state").css({ 'min-width': '30px', 'min-height': '42px'  });
+}
+    // Your code here...
+})();
