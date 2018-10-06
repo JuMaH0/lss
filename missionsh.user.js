87e59cfd802abafa320769de11228fbef56dbc32
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2018-10-06 03:37:41 +0200

    update to 1.5
    update to 1.5

--- missionsh.user.js
+++ missionsh.user.js
@@ -1,105 +1,34 @@
 // ==UserScript==
 // @name         MissionSH
-// @version      1.0
+// @version      1.5
 // @description  Einsätze anzeigen oder ausblenden anhand der Labelfarben grün, gelb, rot
-// @author       JuMaHo
+// @author       JuMaHo & Jan (KBOE2)
 // @include      *://www.leitstellenspiel.de/
 // @grant        none
-// @namespace 	 https://github.com/JuMaH0/lss/raw/master/missionsh.user.js
+// @namespace      https://github.com/JuMaH0/lss/raw/master/missionsh.user.js
 // ==/UserScript==
-
 (function() {
-
-    var circle = 'width: 20px; height: 20px; border: 1px solid black; text-align: center; border-radius: 20px;';
-
-    $(".navbar-right").append('<li><a id="green"><div id="green_circle" style="background-color: #32cd32;' + circle + '"></div></a></li>');
-    $(".navbar-right").append('<li><a id="yellow"><div id="yellow_circle" style="background-color: #fedc32;' + circle + '"></div></a></li>');
-    $(".navbar-right").append('<li><a id="red"><div id="red_circle" style="background-color: #c9302c;' + circle + '"></div></a></li>');
-
-    $(".mission_panel_green").css({ 'display': "block" });
-    $(".mission_panel_yellow").css({ 'display': "block" });
-    $(".mission_panel_red").css({ 'display': "block" });
-
-    var tid = setInterval(mycode, 20000);
-    function mycode() {
-
-        var status_green = document.getElementById('green_circle').style.backgroundColor;
-        var status_yellow = document.getElementById('yellow_circle').style.backgroundColor;
-        var status_red = document.getElementById('red_circle').style.backgroundColor;
-
-
-        if(status_green === 'rgb(211, 211, 211)')
-        {
-            $(".mission_panel_green").css({ 'display': "none" });
-        }
-        else
-        {
-            $(".mission_panel_green").css({ 'display': "block" });
-        }
-
-
-        if(status_yellow === 'rgb(211, 211, 211)')
-        {
-            $(".mission_panel_yellow").css({ 'display': "none" });
-        }
-        else
-        {
-            $(".mission_panel_yellow").css({ 'display': "block" });
-        }
-
-
-        if(status_red === 'rgb(211, 211, 211)')
-        {
-            $(".mission_panel_red").css({ 'display': "none" });
-        }
-        else
-        {
-            $(".mission_panel_red").css({ 'display': "block" });
-        }
-
-
+    const colors = {
+        "green": "rgb(50, 205, 50)",
+        "yellow": "rgb(254, 220, 50)",
+        "red": "rgb(201, 48, 44)"
     }
-
-    $("#green").click(function() {
-        if($('.mission_panel_green').css('display') === 'block')
-        {
-            $(".mission_panel_green").css({ 'display':'none'});
-            $("#green_circle").css({ 'background-color':'#D3D3D3'});
-
-        }
-        else
-        {
-            $(".mission_panel_green").css({ 'display':'block'});
-            $("#green_circle").css({ 'background-color':'#32cd32'});
-        }
+    $('head').append('<style>.circle{width: 20px; height: 20px; border: 1px solid black; text-align: center; border-radius: 20px;}</style>');
+    $.each(colors, function(color, code) {
+        $('head').append('<style>.circle.circle-' + color + '[status="enabled"]{background-color: ' + code + ';}</style>');
+        $('head').append('<style>.circle.circle-' + color + '[status="disabled"]{ background: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' version=\'1.1\' preserveAspectRatio=\'none\' viewBox=\'0 0 100 100\' style=\'background-color: ' + code + ';\'><path d=\'M100 0 L0 100\' stroke=\'black\' stroke-width=\'1\'/><path d=\'M0 0 L100 100\' stroke=\'black\' stroke-width=\'2\'/></svg>");\nbackground-repeat:no-repeat;\nbackground-position:center center;\nbackground-size: 100% 100%, auto;}</style>');
+        $(".navbar-right").append('<li><a id="' + color + '" class="switchMissionStateView"><div id="' + color + '_circle" class="circle circle-' + color + '" status="enabled" color="' + color + '"></div></a></li>');
     });
-
-
-    $("#yellow").click(function() {
-        if($('.mission_panel_yellow').css('display') === 'block')
-        {
-            $(".mission_panel_yellow").css({ 'display':'none'});
-            $("#yellow_circle").css({ 'background-color':'#D3D3D3'});
-        }
-        else
-        {
-            $(".mission_panel_yellow").css({ 'display':'block'});
-            $("#yellow_circle").css({ 'background-color':'#fedc32'});
-        }
+    setInterval(function(){
+        $('.circle').each(function() {
+            let color = $(this).attr("color");
+            $('#' + color + '_circle').attr('status') === "enabled" ? $('.mission_panel_' + color).parent().show() : $('.mission_panel_' + color).parent().hide();
+            $('#' + color + '_circle').attr('status') === "enabled" ? $(".leaflet-interactive[src*='" + color + "_images']").show() : $(".leaflet-interactive[src*='" + color + "_images']").hide();
+        });
+    }, 20000);
+    $('.switchMissionStateView').click(function() {
+        $(this).children().attr('status') === "enabled" ? $(".mission_panel_" + this.id).parent().hide() : $(".mission_panel_" + this.id).parent().show();
+        $('#' + this.id + '_circle').attr('status') === "enabled" ? $(".leaflet-interactive[src*='" + this.id + "_images']").hide() : $(".leaflet-interactive[src*='" + this.id + "_images']").show();
+        $(this).children().attr('status') === "enabled" ? $(this).children().attr('status', 'disabled') : $(this).children().attr('status', 'enabled');
     });
-
-
-    $("#red").click(function() {
-        if($('.mission_panel_red').css('display') === 'block')
-        {
-            $(".mission_panel_red").css({ 'display':'none'});
-            $("#red_circle").css({ 'background-color':'#D3D3D3'});
-        }
-        else
-        {
-            $(".mission_panel_red").css({ 'display':'block'});
-            $("#red_circle").css({ 'background-color':'#c9302c'});
-        }
-    });
-
 })();
