388a5bf32d1a476a69a98a5d9cc29cc3315957f1
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2018-10-10 18:06:37 +0200

    update
    update

--- missionshX.user.js
+++ missionshX.user.js
@@ -7,113 +7,171 @@
 // @grant        none
 // @namespace      https://github.com/JuMaH0/lss/raw/master/missionsh.user.js
 // ==/UserScript==
- (function() {
+(function() {
+
+    const showmap = 1; // 1 = Icons auf map ausblenden 0 = Icons nicht ausblenden
 
     var circle = 'width: 20px; height: 20px; border: 1px solid black; text-align: center; border-radius: 20px;';
     $(".navbar-right").append('<li><a id="green"><div id="green_circle" style="background-color: #32cd32;' + circle + '"></div></a></li>');
     $(".navbar-right").append('<li><a id="yellow"><div id="yellow_circle" style="background-color: #fedc32;' + circle + '"></div></a></li>');
     $(".navbar-right").append('<li><a id="red"><div id="red_circle" style="background-color: #c9302c;' + circle + '"></div></a></li>');
-    $(".mission_panel_green").css({ 'display': "block" });
-    $(".mission_panel_yellow").css({ 'display': "block" });
-    $(".mission_panel_red").css({ 'display': "block" });
+    
+	$(".mission_panel_green").css({
+        'display': "block"
+    });
+    $(".mission_panel_yellow").css({
+        'display': "block"
+    });
+    $(".mission_panel_red").css({
+        'display': "block"
+    });
 
-     var tid = setInterval(mycode, 5000);
+    var tid = setInterval(mycode, 5000);
+
     function mycode() {
 
 
         let missionDeleteOrigin = missionDelete;
-        missionDelete=function(e){missionDeleteOrigin(e),$("#mission_"+e).addClass("finished")};
+        missionDelete = function(e) {
+            missionDeleteOrigin(e), $("#mission_" + e).addClass("finished")
+        };
 
         var status_green = document.getElementById('green_circle').style.backgroundColor;
         var status_yellow = document.getElementById('yellow_circle').style.backgroundColor;
         var status_red = document.getElementById('red_circle').style.backgroundColor;
-        if(status_green === 'rgb(211, 211, 211)')
-        {
-            $(".mission_panel_green").css({ 'display': "none" });
-            $(".leaflet-interactive[src*='green_images']").hide();
-			$(".leaflet-interactive[src*='gruen']").hide();
+       
+	   if (status_green === 'rgb(211, 211, 211)') {
+            $(".mission_panel_green").css({
+                'display': "none"
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='green_images']").hide();
+                $(".leaflet-interactive[src*='gruen']").hide();
+            }
 
+        } else {
+            $(".mission_panel_green").css({
+                'display': "block"
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='green_images']").show();
+                $(".leaflet-interactive[src*='gruen']").show();
+            }
         }
-        else
-        {
-            $(".mission_panel_green").css({ 'display': "block" });
-            $(".leaflet-interactive[src*='green_images']").show();
-			$(".leaflet-interactive[src*='gruen']").show();
-        }
-        if(status_yellow === 'rgb(211, 211, 211)')
-        {
-            $(".mission_panel_yellow").css({ 'display': "none" });
-            $(".leaflet-interactive[src*='yellow_images']").hide();
-			$(".leaflet-interactive[src*='gelb']").hide();
+		
+        if (status_yellow === 'rgb(211, 211, 211)') {
+            $(".mission_panel_yellow").css({
+                'display': "none"
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='yellow_images']").hide();
+                $(".leaflet-interactive[src*='gelb']").hide();
+            }
 
+        } else {
+            $(".mission_panel_yellow").css({
+                'display': "block"
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='yellow_images']").show();
+                $(".leaflet-interactive[src*='gelb']").show();
+            }
         }
-        else
-        {
-            $(".mission_panel_yellow").css({ 'display': "block" });
-            $(".leaflet-interactive[src*='yellow_images']").show();
-			$(".leaflet-interactive[src*='gelb']").show();
-        }
-        if(status_red === 'rgb(211, 211, 211)')
-        {
-            $(".mission_panel_red").css({ 'display': "none" });
-            $(".leaflet-interactive[src*='red_images']").hide();
-			$(".leaflet-interactive[src*='rot']").hide();
-			
+        if (status_red === 'rgb(211, 211, 211)') {
+            $(".mission_panel_red").css({
+                'display': "none"
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='red_images']").hide();
+                $(".leaflet-interactive[src*='rot']").hide();
+            }
 
-        }
-        else
-        {
-            $(".mission_panel_red").css({ 'display': "block" });
-            $(".leaflet-interactive[src*='red_images']").show();
-			$(".leaflet-interactive[src*='rot']").show();
+		} else {
+            $(".mission_panel_red").css({
+                'display': "block"
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='red_images']").show();
+                $(".leaflet-interactive[src*='rot']").show();
+            }
         }
     }
     $("#green").click(function() {
-        if($('.mission_panel_green').css('display') === 'block')
-        {
-            $(".mission_panel_green").css({ 'display':'none'});
-            $("#green_circle").css({ 'background-color':'#D3D3D3'});
-            $(".leaflet-interactive[src*='green_images']").hide();
-            $(".leaflet-interactive[src*='gruen']").hide();
-        }
-        else
-        {
-            $(".mission_panel_green").css({ 'display':'block'});
-            $("#green_circle").css({ 'background-color':'#32cd32'});
-            $(".leaflet-interactive[src*='green_images']").show();
-            $(".leaflet-interactive[src*='gruen']").show();
+        if ($('.mission_panel_green').css('display') === 'block') {
+            $(".mission_panel_green").css({
+                'display': 'none'
+            });
+            $("#green_circle").css({
+                'background-color': '#D3D3D3'
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='green_images']").hide();
+                $(".leaflet-interactive[src*='gruen']").hide();
+            }
+			
+        } else {
+            $(".mission_panel_green").css({
+                'display': 'block'
+            });
+            $("#green_circle").css({
+                'background-color': '#32cd32'
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='green_images']").show();
+                $(".leaflet-interactive[src*='gruen']").show();
+            }
         }
     });
     $("#yellow").click(function() {
-        if($('.mission_panel_yellow').css('display') === 'block')
-        {
-            $(".mission_panel_yellow").css({ 'display':'none'});
-            $("#yellow_circle").css({ 'background-color':'#D3D3D3'});
-            $(".leaflet-interactive[src*='yellow_images']").hide();
-            $(".leaflet-interactive[src*='gelb']").hide();
-        }
-        else
-        {
-            $(".mission_panel_yellow").css({ 'display':'block'});
-            $("#yellow_circle").css({ 'background-color':'#fedc32'});
-            $(".leaflet-interactive[src*='yellow_images']").show();
-            $(".leaflet-interactive[src*='gelb']").show();
+        if ($('.mission_panel_yellow').css('display') === 'block') {
+            $(".mission_panel_yellow").css({
+                'display': 'none'
+            });
+            $("#yellow_circle").css({
+                'background-color': '#D3D3D3'
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='yellow_images']").hide();
+                $(".leaflet-interactive[src*='gelb']").hide();
+            }
+			
+        } else {
+            $(".mission_panel_yellow").css({
+                'display': 'block'
+            });
+            $("#yellow_circle").css({
+                'background-color': '#fedc32'
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='yellow_images']").show();
+                $(".leaflet-interactive[src*='gelb']").show();
+            }
         }
     });
     $("#red").click(function() {
-        if($('.mission_panel_red').css('display') === 'block')
-        {
-            $(".mission_panel_red").css({ 'display':'none'});
-            $("#red_circle").css({ 'background-color':'#D3D3D3'});
-            $(".leaflet-interactive[src*='red_images']").hide();
-            $(".leaflet-interactive[src*='rot']").hide();
-}
-        else
-        {
-            $(".mission_panel_red").css({ 'display':'block'});
-            $("#red_circle").css({ 'background-color':'#c9302c'});
-            $(".leaflet-interactive[src*='red_images']").show();
-            $(".leaflet-interactive[src*='rot']").show();
+        if ($('.mission_panel_red').css('display') === 'block') {
+            $(".mission_panel_red").css({
+                'display': 'none'
+            });
+            $("#red_circle").css({
+                'background-color': '#D3D3D3'
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='red_images']").hide();
+                $(".leaflet-interactive[src*='rot']").hide();
+            }
+			
+        } else {
+            $(".mission_panel_red").css({
+                'display': 'block'
+            });
+            $("#red_circle").css({
+                'background-color': '#c9302c'
+            });
+            if (showmap === 1) {
+                $(".leaflet-interactive[src*='red_images']").show();
+                $(".leaflet-interactive[src*='rot']").show();
+            }
         }
     });
 })();
