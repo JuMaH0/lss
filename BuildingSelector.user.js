a74016a0e66f747d4ea45edf0656de7af69f00ce
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2018-10-25 17:08:51 +0200

    push
    push

---
+++ BuildingSelector.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         BuildingSelector
+// @namespace    http://tampermonkey.net/
+// @version      1.0.5
+// @description  Gebäudeliste nur LST anzeigen
+// @author       JuMaHo
+// @match        https://www.leitstellenspiel.de/*
+// ==/UserScript==
+
+(function() {
+
+    var group = [ "feuerwehr", "rettung", "polizei", "thw", "wasserrettung", "schule" ];
+    $.each( group, function( i, value ) {
+
+ if (!$("#building_selection_" + value + "").hasClass("btn-danger")) {$( "#building_selection_" + value + "" )[0].click(); }
+
+});
+})();
