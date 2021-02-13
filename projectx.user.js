d8668bce41ab7cecceb3c5c53d4ec520598daffa
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2021-02-14 00:59:38 +0100

    Update projectx.user.js

--- projectx.user.js
+++ projectx.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Projekt X
-// @version      0.1
+// @version      0.2
 // @description  More detailed mission description
 // @author       JuMaHo
 // @match        https://www.leitstellenspiel.de/missions/*
@@ -14,8 +14,7 @@
     var div = document.getElementById('missiondetails');
 
     var mission = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
-    var adress = $('#mission_help').attr('href').split("=").pop().replace(/\?.*/, '');
-    var innenTeil = document.getElementById('mission_general_info').innerHTML;
+    var address = $('#mission_general_info').children('small').text().split('|')[0].trim();
 
     var genderarray = ['Herr', 'Frau'];
     var gender = genderarray[Math.floor(Math.random() * genderarray.length)];
@@ -26,22 +25,11 @@
     var array476 = ['hier wurde ein Fahhradfahrer angefahren', 'hier liegt ein Fahrradfahrer auf der Straße', 'ich habe einen Fahrradfahrer angefahren', 'hier wurde ein Radfahrer umgemäht'];
     var mission476 = array476[Math.floor(Math.random() * array476.length)];
 
-    var res = innenTeil.split(">");
-    var mos = res[8].split(",");
-    var pos = mos[1].split("|");
-    var kos = pos[0].split("<");
-
-    var address1 = mos[0].split(" ");
-    var address2 = kos[0].split(" ");
-
-    if (address1[7] === undefined) {
-        address1[7] = '';
-    }
-    if (address1[8] === undefined) {
-        address1[8] = '';
-    }
+    var addressx = address.split(",");
+    var addressa = addressx[0];
+    var addressb = addressx[1].replace(/\d+/g, '');
 
     if (mission === '476') {
-        div.innerHTML += '' + gender + ' ' + name + ' hier, schicken Sie schnell jemanden in die ' + address1[6] + ' ' + address1[7] + ' ' + address1[8] + ' nach ' + address2[2] + ' ' + address2[3] + ', ' + mission476 + '';
+        div.innerHTML += '' + gender + ' ' + name + ' hier, schicken Sie schnell jemanden in die ' + addressa + ' nach ' + addressb + ', ' + mission476 + '';
     }
 })();
