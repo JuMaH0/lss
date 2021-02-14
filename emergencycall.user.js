bf773c8c3bf904aa9e400af997b9d6f548b4d2fd
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2021-02-14 11:16:23 +0100

    Update emergencycall.user.js

--- emergencycall.user.js
+++ emergencycall.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         Projekt X
-// @version      0.4
-// @description  More detailed mission description
+// @name         EmergencyCall
+// @version      0.0.4
+// @description  dieses Script generiert eine zufällige Einsatzmeldung
 // @author       JuMaHo
 // @match        https://www.leitstellenspiel.de/missions/*
-// @grant        none
+// @updateURL    https://github.com/JuMaH0/lss/raw/master/emergencycall.user.js
+// @downloadURL  https://github.com/JuMaH0/lss/raw/master/emergencycall.user.js
 // ==/UserScript==
 
 (function () {
@@ -14,17 +15,15 @@
     var div = document.getElementById('missiondetails');
 
     var mission = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
-    var address = $('#mission_general_info').children('small').text().split('|')[0].trim();
-
-    var genderarray = ['Herr', 'Frau'];
+    
+    var genderarray = ['','Herr', 'Frau'];
     var gender = genderarray[Math.floor(Math.random() * genderarray.length)];
 
     var namearray = ['Meier', 'Müller', 'Schmitt'];
     var name = namearray[Math.floor(Math.random() * namearray.length)];
 
-    var message_mainarray = ['bitte kommen Sie in die', 'bitte schicken sie jemanden in die', 'kommen Sie schnell zur'];
+    var message_mainarray = ['bitte kommen Sie schnell', 'bitte schicken Sie jemanden vorbei', 'wir brauchen hilfe'];
     var message_main = message_mainarray[Math.floor(Math.random() * message_mainarray.length)];
-
 
 
     var array145 = ['hier fährt einer Schlangenlinie, ich glaub der ist besoffen', 'da fährt jemand total besoffen mit dem Auto'];
@@ -34,14 +33,9 @@
     var xmission476 = array476[Math.floor(Math.random() * array476.length)];
 
 
-
-    var addressx = address.split(",");
-    var addressa = addressx[0];
-    var addressb = addressx[1].replace(/\d+/g, '');
-
     function emergency_call(message, xmissionx) {
         $('#missiondetails').css('display', 'block');
-        div.innerHTML += '' + gender + ' ' + name + ' hier, ' + message + ' ' + addressa + ' nach ' + addressb + ', ' + xmissionx + '';
+        div.innerHTML += '' + gender + ' ' + name + ' hier, ' + message + ', ' + xmissionx + '';
     }
 
     if ($(".glyphicon-user").length <= 0) {
