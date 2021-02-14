3171e0afa18b453b51ba7a69ed3d1dcc268685a3
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2021-02-14 14:49:43 +0100

    BMA checked
    checked is emergency triggered from BMA

--- emergencycall.user.js
+++ emergencycall.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         EmergencyCall
-// @version      0.0.4
+// @version      0.0.6
 // @description  dieses Script generiert eine zufällige Einsatzmeldung
 // @author       JuMaHo
 // @match        https://www.leitstellenspiel.de/missions/*
@@ -10,13 +10,14 @@
 
 (function () {
     'use strict';
-
+    
     $('<div id="missiondetails" style="background-color: #e89a0b; margin-bottom: 10px; padding: 5px; display: none; border: 1px solid #000;"></div>').prependTo('#col_left');
     var div = document.getElementById('missiondetails');
 
     var mission = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
+    var checkbma = document.getElementById("missionH1").innerHTML;
     
-    var genderarray = ['','Herr', 'Frau'];
+    var genderarray = ['', 'Herr', 'Frau'];
     var gender = genderarray[Math.floor(Math.random() * genderarray.length)];
 
     var namearray = ['Meier', 'Müller', 'Schmitt'];
@@ -25,13 +26,11 @@
     var message_mainarray = ['bitte kommen Sie schnell', 'bitte schicken Sie jemanden vorbei', 'wir brauchen hilfe'];
     var message_main = message_mainarray[Math.floor(Math.random() * message_mainarray.length)];
 
-
     var array145 = ['hier fährt einer Schlangenlinie, ich glaub der ist besoffen', 'da fährt jemand total besoffen mit dem Auto'];
     var xmission145 = array145[Math.floor(Math.random() * array145.length)];
 
     var array476 = ['hier wurde ein Fahhradfahrer angefahren', 'hier liegt ein Fahrradfahrer auf der Straße', 'ich habe einen Fahrradfahrer angefahren', 'hier wurde ein Radfahrer umgemäht'];
     var xmission476 = array476[Math.floor(Math.random() * array476.length)];
-
 
     function emergency_call(message, xmissionx) {
         $('#missiondetails').css('display', 'block');
@@ -39,10 +38,15 @@
     }
 
     if ($(".glyphicon-user").length <= 0) {
+        if (checkbma.includes('(Brandmeldeanlage)')) {
+            $('#missiondetails').css('display', 'block');
+            div.innerHTML += 'Brandmeldeanlage aufgelaufen';
+            return;
+        }
         if (mission === '145')
-        emergency_call(message_main, xmission145)
-        if (mission === '476')
-        emergency_call(message_main, xmission476)
+            emergency_call(message_main, xmission145)
+            if (mission === '476')
+            emergency_call(message_main, xmission476)
     }
 
 })();
