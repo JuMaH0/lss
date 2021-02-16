53052fa82c23f0d019d3b3064f8f34e9d817f1dd
JuMaH0 <38581280+JuMaH0@users.noreply.github.com>
2021-02-16 19:29:21 +0100

    Update emergencycall.user.js

--- emergencycall.user.js
+++ emergencycall.user.js
@@ -10,48 +10,35 @@
 
 (function () {
     'use strict';
-    
-    $('<div id="missiondetails" style="background-color: #e89a0b; margin-bottom: 10px; padding: 5px; display: none; border: 1px solid #000; font-size: 15px;"></div>').prependTo('#col_left');
-    var div = document.getElementById('missiondetails');
-
-    var mission = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
-    var checkbma = document.getElementById("missionH1").innerHTML;
-    
-    var genderarray = ['', 'Herr', 'Frau'];
-    var gender = genderarray[Math.floor(Math.random() * genderarray.length)];
-
-    var namearray = ['Meier', 'Müller', 'Schmitt', 'Wagner', 'Richter', 'Schneider', 'Werner', 'Jung'];
-    var name = namearray[Math.floor(Math.random() * namearray.length)];
-
-    var message_mainarray = ['bitte kommen Sie schnell', 'bitte schicken Sie jemanden vorbei', 'wir brauchen hilfe'];
-    var message_main = message_mainarray[Math.floor(Math.random() * message_mainarray.length)];
-
-    var array145 = ['hier fährt einer Schlangenlinie, ich glaub der ist besoffen', 'da fährt jemand total besoffen mit dem Auto'];
-    var xmission145 = array145[Math.floor(Math.random() * array145.length)];
-
-    var array164 = ['mein Kind ist auf den Kopf gefallen und blutet nun stark.'];
-    var xmission164 = array164[Math.floor(Math.random() * array164.length)];
-
-    var array476 = ['hier wurde ein Fahhradfahrer angefahren', 'hier liegt ein Fahrradfahrer auf der Straße', 'ich habe einen Fahrradfahrer angefahren', 'hier wurde ein Radfahrer umgemäht'];
-    var xmission476 = array476[Math.floor(Math.random() * array476.length)];
-
-    function emergency_call(message, xmissionx) {
-        $('#missiondetails').css('display', 'block');
-        div.innerHTML += '' + gender + ' ' + name + ' hier, ' + message + ', ' + xmissionx + '';
-    }
-
-    if ($(".glyphicon-user").length <= 0) {
-        if (checkbma.includes('(Brandmeldeanlage)')) {
-            $('#missiondetails').css('display', 'block');
-            div.innerHTML += 'Brandmeldeanlage aufgelaufen';
-            return;
+    let github_link = 'https://jumah0.github.io/json/lss/emergencycall.json';
+    $.getJSON(github_link, function (data) {
+        var mission = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
+        var checkbma = document.getElementById("missionH1").innerHTML;
+       
+        for (var i = 0; i < data.mission.length; i++) {
+            if (mission === data.mission[i].mission_id) {
+                var emergency_txt = data.mission[i].mission_text;
+            }
         }
-            if (mission === '145')
-            emergency_call(message_main, xmission145)
-            if (mission === '164')
-            emergency_call(message_main, xmission164)
-            if (mission === '476')
-            emergency_call(message_main, xmission476)
-    }
-
+        $('<div id="missiondetails" style="background-color: #e89a0b; margin-bottom: 10px; padding: 5px; display: none; border: 1px solid #000; font-size: 15px;"></div>').prependTo('#col_left');
+        var div = document.getElementById('missiondetails');
+        var genderarray = ['', 'Herr', 'Frau'];
+        var gender = genderarray[Math.floor(Math.random() * genderarray.length)];
+        var namearray = data.caller.caller_names;
+        var name = namearray[Math.floor(Math.random() * namearray.length)];
+        var emergency_textarray = emergency_txt;
+        var emergency_text = emergency_textarray[Math.floor(Math.random() * emergency_textarray.length)];
+        function emergency_call(xmissionx) {
+            $('#missiondetails').css('display', 'block');
+            div.innerHTML += '' + gender + ' ' + name + ' hier, ' + xmissionx + '';
+        }
+         if ($(".glyphicon-user").length <= 0) {
+            if (checkbma.includes('(Brandmeldeanlage)')) {
+                $('#missiondetails').css('display', 'block');
+                div.innerHTML += 'Brandmeldeanlage aufgelaufen';
+                return;
+            }
+        }
+        emergency_call(emergency_text)
+    });
 })();
